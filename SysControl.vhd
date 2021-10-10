2d26bff1350ffc5dc606baf01edf89d1db44adfa
Cameron Young <cameronky99@gmail.com>
2021-10-10 16:51:46 -0500

    Added a state machine with flags and outputs

--- SysControl.vhd
+++ SysControl.vhd
@@ -6,44 +6,36 @@ use IEEE.std_logic_1164.all;
 --proper state machine implementation.
 
 entity StateMachine is 
-	port(X, Y, CLK: in std_logic;
-		Z: out std_logic_vector(1 downto 0));
+	port(MemCheck, CalcDone, StoreDone, CLK: in std_logic;
+		Load_Mem, Calc_Start, Store_Mem: out std_logic);
 end StateMachine;
 
 architecture structure of StateMachine is
-signal State: integer range 0 to 3 := 0;
+signal State: integer range 0 to 2 := 0;
 begin 
 	process(CLK)
 	begin
 		if CLK'event and CLK = '1' then 
 			case State is
-				when 0 => 
-					if X = '0' and Y = '0' then State <= 0;
-					elsif X = '0' and Y = '1' then State <= 1;
-					elsif X = '1' and Y = '0' then State <= 2;
-					elsif X = '1' and Y = '1' then State <= 1;
+				when 0 => --Wait State, waits until memory has input matrices
+					if  MemCheck = '1' then State <= 1;
+					elsif MemCheck = '0' then State <= 0;
+					end if
+				when 1 => --Calc State, waits until memory is found then triggers the calculation state
+					if CalcDone = '1' then State <= 2;
+					elsif CalcDone = '0' then State <= 1;
 					end if;
-				when 1 =>
-					if X = '0' and Y = '0' then State <= 1;
-					elsif X = '0' and Y = '1' then State <= 0;
-					elsif X = '1' and Y = '0' then State <= 2;
-					elsif X = '1' and Y = '1' then State <= 3;
-					end if;
-				when 2 =>
-					if X = '0' and Y = '0' then State <= 2;
-					elsif X = '0' and Y = '1' then State <= 3;
-					elsif X = '1' and Y = '0' then State <= 3;
-					elsif X = '1' and Y = '1' then State <= 1;
-					end if;
-				when 3 =>
-					if X = '0' and Y = '0' then State <= 3;
-					elsif X = '0' and Y = '1' then State <= 0;
-					elsif X = '1' and Y = '0' then State <= 1;
-					elsif X = '1' and Y = '1' then State <= 0;
+				when 2 => --Done State, Checks if the calculations are done and signals to store the result and start looking for more inputs.
+					if StoreDone = '1' then State <= 0;
+					elsif StoreDone = '0' then State <= 2;
 					end if;
 			end case;
 		end if;
 	end process;
-	Z <= '1' when (State = 2) or (State = 3)
+	Load_Mem <= '1' when (State = 0) and (MemCheck = '0')
+		else '0';
+	Calc_Start <= '1' when (State = 1) and (CalcDone = '0')
+		else '0';
+	Store_Mem <= '1' when (State = 2) and (StoreDone = '0')
 		else '0';
 end structure;
