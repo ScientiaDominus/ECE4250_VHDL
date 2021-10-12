872784ebe45da5339984901fa8f8bfa9782570b3
Cameron Young <cameronky99@gmail.com>
2021-10-12 11:57:14 -0500

    Added PE untested but compiling

--- ProcessingElement.vhd
+++ ProcessingElement.vhd
@@ -3,42 +3,33 @@ use IEEE.std_logic_1164.all;
 
 entity PE is
     port(load, clr, clk: in std_logic;
-        Mcand, Mlier: in std_logic_vector(7 downto 0);
-        addIn: in std_logic_vector(24 downto 0);
-        Mout: out std_logic_vector(7 downto 0);
-        Sout: out std_logic_vector(24 downto 0));
+        Mcand, Mlier: in integer;
+        addIn: in integer;
+        Mout: out integer;
+        Sout: out integer);
 end PE;
 
 architecture Structure of PE is
-    
-    component 8bitRegister 
-        port(d: in std_logic_vector (7 downto 0);
-        ld, clr, clk: in std_logic;
-        q: out std_logic_vector (7 downto 0));
-    end component;
-    component 24bitRegister
-    
-    end component;
-    component 8bitMultiplier
-
-    end component;
-    component 24bitAdder
-        port(X,Y: in std_logic_vector(23 downto 0);
-        Ci: in std_logic;
-        S: out std_logic_vector(23 downto 0);
-        Co: out std_logic);
-    end component;
+    signal MandReg: integer;
+    signal MierReg: integer;
+    signal Multiplied: integer;
+    signal AddReg: integer;
     begin
-    signal MResult: std_logic_vector(15 downto 0);
-    signal AResult: std_logic_vector(23 downto 0);
-    RA1: 8bitRegister port map(Mcand, load, clr, clk, ); --Which port should these be connected to? Perhaps another signal is needed? 
-    RA2: 8bitRegister port map(Mlier, load, clr, clk, ); --RA1 should definitely be connected to Mout but how is it also connected to Min for the 8bitMultiplier?
-    RA3: 24bitRegister port map()
-
-    FA1: 24bitAdder port map(addIn, ("00000000" & MResult), addIn(24), Aresult(23 downto 0), AResult(24));
-    process(clk)
-        begin  
-            if rising_edge(clk) then
-    end;
-    Mout <= Mcand;
+    process(clk, clr)
+        begin
+        if clr = '1' then
+            MandReg <= 0;
+            MierReg <= 0;
+        elsif rising_edge(clk) then
+            if load = '1' then
+                MandReg <= Mcand;
+                MierReg <= Mlier;
+            end if;
+            Mout <= MandReg;
+            Multiplied <= MandReg * MierReg;
+            AddReg <= addIn + Multiplied;
+            Sout <= AddReg;
+        end if; 
+        Mout <= Mcand;
+    end process;
 end Structure;
